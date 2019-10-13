ff8050f7c7502b13de359dd1b3ee20469adc9508
ChrisChewning <chewning.cl@gmail.com>
2019-10-13 08:51:11 -0500

    link, link text, and summaries displaying from washington post

--- newsfeed.py
+++ newsfeed.py
@@ -9,23 +9,24 @@ source = requests.get('https://www.washingtonpost.com/').text
 soup = BeautifulSoup(source, 'lxml')
 
 front_page = soup.find("section", {"id": "main-content"})
-headlines = front_page.findAll(class_='headline', limit=10)
-summaries = front_page.findAll(class_='no-skin flex-item flex-stack normal-air text-align-left equalize-height-target', limit=10)         
+headlines = front_page.findAll(class_='headline', limit=1)
+summaries = front_page.findAll(class_='no-skin flex-item flex-stack normal-air text-align-left equalize-height-target', limit=1)         
 
 
-i = 0
-for h in headlines:
-    print(h.text)
-    link = h.find('a')
-    print(link['href'])
-    article = summaries[i].text
-    print(article)
-    i += 1    
+from flask import Flask, render_template
+from bs4 import BeautifulSoup, SoupStrainer
+import requests  #from python library
+import urllib.request as urllib2
 
-for s in summaries:
-    for h in headlines:
-        print(h.text)
-        link = h.find('a')
-        print(link['href'])
-        print(s.text)
+
+source = requests.get('https://www.washingtonpost.com/').text
+soup = BeautifulSoup(source, 'lxml')
+
+articles = front_page.findAll(class_='no-skin flex-item flex-stack normal-air text-align-left equalize-height-target', limit=10)         
+
+for article in articles:
+    link = article.find('a')
+    print('link text: ', link.text)
+    print('link: ', link['href'])
+    print('summary: ', article.text)
 

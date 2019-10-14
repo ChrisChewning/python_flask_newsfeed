67838d6f49b3d8ea85b9d4bb05893aa0928bcca5
ChrisChewning <chewning.cl@gmail.com>
2019-10-14 00:22:16 -0500

    fixed summary issue

--- newsfeed.py
+++ newsfeed.py
@@ -9,24 +9,25 @@ source = requests.get('https://www.washingtonpost.com/').text
 soup = BeautifulSoup(source, 'lxml')
 
 front_page = soup.find("section", {"id": "main-content"})
-headlines = front_page.findAll(class_='headline', limit=1)
-summaries = front_page.findAll(class_='no-skin flex-item flex-stack normal-air text-align-left equalize-height-target', limit=1)         
+articles = front_page.findAll(True, {'class':['no-skin flex-item flex-stack normal-air text-align-left wrap-text equalize-height-target', 'no-skin flex-item flex-stack normal-air text-align-left equalize-height-target']}, limit=10)       
 
-
-from flask import Flask, render_template
-from bs4 import BeautifulSoup, SoupStrainer
-import requests  #from python library
-import urllib.request as urllib2
-
-
-source = requests.get('https://www.washingtonpost.com/').text
-soup = BeautifulSoup(source, 'lxml')
-
-articles = front_page.findAll(class_='no-skin flex-item flex-stack normal-air text-align-left equalize-height-target', limit=10)         
 
 for article in articles:
     link = article.find('a')
-    print('link text: ', link.text)
-    print('link: ', link['href'])
-    print('summary: ', article.text)
+    link_text = link.text
+    link_url = link['href']
+    print(link_text)
+    print(link_url)
+
+    if article.find(class_="blurb normal normal-style"):
+        print(article.text)
+    else:
+        print("No summary given")
+    
+
+#this grabs the summary without the extra information:
+
+# headline_text = front_page.findAll(class_='blurb normal normal-style', limit=10)
+    # for h in headline_text:
+#     print(h.text)
 

cc9b718917037f494a6feba1a2db83eecd9feae9
ChrisChewning <chewning.cl@gmail.com>
2019-10-14 23:21:18 -0500

    refactored

--- venv/app/newsfeed.py
+++ venv/app/newsfeed.py
@@ -1,2 +1,40 @@
 from app import app #importa pp variable, which is a member of the app package
+from flask import Flask, render_template
+from bs4 import BeautifulSoup, SoupStrainer
+import requests  #from python library
+import urllib.request as urllib2
+
+
+source = requests.get('https://www.washingtonpost.com/').text
+#lxml is the markup
+soup = BeautifulSoup(source, 'lxml')
+
+front_page = soup.find("section", {"id": "main-content"})
+articles = front_page.findAll(True, {'class':['no-skin flex-item flex-stack normal-air text-align-left wrap-text equalize-height-target', 'no-skin flex-item flex-stack normal-air text-align-left equalize-height-target']}, limit=10)
+
+def getWaPo():
+    l = []
+    for article in articles:
+        a = { }
+        link = article.find('a')
+        a['link_text'] = link.text
+        a['link_url'] = link['href']
+        # print(a['link_text'])
+        # print(a['link_url'])
+        print(a)
+        if article.find(class_="blurb normal normal-style"):
+            a['link_summary'] = article.text
+        else:
+            a['link_summary'] = 'No summary given'
+        l.append(a)
+    return l
+    
+
+
+
+#this grabs the summary without the extra information:
+
+# headline_text = front_page.findAll(class_='blurb normal normal-style', limit=10)
+    # for h in headline_text:
+#     print(h.text)
 
--- venv/app/routes.py
+++ venv/app/routes.py
@@ -1,6 +1,30 @@
-from app import app
+from flask import render_template
+from app import app #import app variable, which is a member of the app package
+from newsfeed import getWaPo
+from flask import Flask, render_template, jsonify
+from bs4 import BeautifulSoup, SoupStrainer
+import requests  #from python library
+import urllib.request as urllib2
+
 
 @app.route('/')
-@app.route('/index')
 def index():
-    return "Hello, World!"
+    return jsonify(Products=getWaPo())
+
+    # return render_template('index.html', title="Home")
+
+    #     return jsonify(Products=getWaPo())
+
+
+
+@app.route('/login')
+def login():
+    return render_template('index.html', title='Login')
+
+
+#this grabs the summary without the extra information:
+
+# headline_text = front_page.findAll(class_='blurb normal normal-style', limit=10)
+    # for h in headline_text:
+#     print(h.text)
+

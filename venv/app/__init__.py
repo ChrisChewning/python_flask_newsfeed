b0a1fdacb1b1e9942a1950c84d5fc8aca6842320
ChrisChewning <chewning.cl@gmail.com>
2019-10-16 14:38:21 -0500

    hooked up Bootstrap & CSS

--- venv/app/__init__.py
+++ venv/app/__init__.py
@@ -1,5 +1,9 @@
 from flask import Flask #application object is instance of Flask
-app = Flask(__name__) #set to the name of of the module where it's used. 
+app = Flask(__name__) #set to the name of of the module where it's used. needs to be above from app...
 from app import routes
+from flask_bootstrap import Bootstrap
+
+Bootstrap(app)
+
 
 #this is for handling the app instance
